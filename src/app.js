c97f89445f50fd4b76efa409262b8b3aa0bab551
Mitchell Gritts <mitchellgritts@pm.me>
2021-06-07 15:00:39 -0700

    Add fastify-cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import autoload from 'fastify-autoload'
 import printRoutes from 'fastify-print-routes'
 import fastifyEnv from 'fastify-env'
+import fastifyCors from 'fastify-cors'
 import { join } from 'desm'
 
 const envOptions = {
@@ -18,7 +19,11 @@ const envOptions = {
 export default async function (fastify, opts) {
   fastify.register(fastifyEnv, envOptions)
   fastify.register(printRoutes)
+  fastify.register(fastifyCors, {
+    origin: '*'
+  })
 
+  // loading local plugins
   fastify.register(autoload, {
     dir: join(import.meta.url, './plugins')
   })
