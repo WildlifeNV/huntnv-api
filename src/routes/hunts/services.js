ba21e6c8349c85f954eb3f2b17744f54cd470e34
Mitchell Gritts <mitchellgritts@pm.me>
2022-01-27 08:13:08 -0800

    Docs: document huntFeed service.

--- src/routes/hunts/services.js
+++ src/routes/hunts/services.js
@@ -7,6 +7,14 @@ import { pick, omit, pickTruthy } from '../../utils/objMethods.js'
 export default fp(async (fastify, opts, next) => {
   const hunts = HuntRepo(fastify)
 
+  /**
+   * Returns formatted data from the hunt feeds database request.
+   * @param {{ query: object }} params
+   * @param {number} params.query The query object passed from the route handler.
+   * @returns {Object} data The data, formatted and returned from the database.
+   * @returns {number} data.total_hunts The total number of hunts in the query.
+   * @returns {Array} data.hunt_feed All of the hunts returned by the query.
+   */
   const huntFeed = async ({ query }) => {
     const props = ['success_rate', 'draw_rate']
     const gteProps = pickTruthy(pick(query, props))
